eee4c79d89f428b7cae1d9ce59f5d736ad75f73c
{{coderfromFIT}} <{{narobotu45@gmail.com}}>
2023-10-02 15:41:59 +0300

    Lab3

---
+++ Polygon/Polygon/Polygon/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+
+class Point
+{
+    private double x;
+    private double y;
+    private string label;
+
+    public Point(double x, double y, string label)
+    {
+        this.x = x;
+        this.y = y;
+        this.label = label;
+    }
+
+    public double X
+    {
+        get { return x; }
+    }
+
+    public double Y
+    {
+        get { return y; }
+    }
+
+    public string Label
+    {
+        get { return label; }
+    }
+}
+
+class Figure
+{
+    private Point[] points;
+    private string name;
+
+    public Figure(string name, Point p1, Point p2, Point p3)
+    {
+        this.name = name;
+        points = new Point[] { p1, p2, p3 };
+    }
+
+    public Figure(string name, Point p1, Point p2, Point p3, Point p4)
+    {
+        this.name = name;
+        points = new Point[] { p1, p2, p3, p4 };
+    }
+
+    public Figure(string name, Point p1, Point p2, Point p3, Point p4, Point p5)
+    {
+        this.name = name;
+        points = new Point[] { p1, p2, p3, p4, p5 };
+    }
+
+    private double GetDistance(Point A, Point B)
+    {
+        return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+    }
+
+    public double GetSideLength(Point A, Point B)
+    {
+        return GetDistance(A, B);
+    }
+
+    public void CalculatePerimeter()
+    {
+        double perimeter = 0;
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            perimeter += GetSideLength(points[i], points[i + 1]);
+        }
+        perimeter += GetSideLength(points[points.Length - 1], points[0]);
+
+        Console.WriteLine($"Name of the figure: {name}");
+        Console.WriteLine($"Perimeter of the figure: {perimeter}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Point p1 = new Point(0, 0, "A");
+        Point p2 = new Point(0, 1, "B");
+        Point p3 = new Point(1, 1, "C");
+
+        Figure triangle = new Figure("Triangle", p1, p2, p3);
+        triangle.CalculatePerimeter();
+
+        
+        Console.ReadLine();
+    }
+}
---
+++ Rectangle/Rectangle/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Rectangle
+{
+    // field of class
+    private double side1;
+    private double side2;
+
+    // constructor class
+    public Rectangle(double side1, double side2)
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+
+    
+    public double CalculateArea()
+    {
+        return side1 * side2;
+    }
+
+    
+    public double CalculatePerimeter()
+    {
+        return 2 * (side1 + side2);
+    }
+
+    
+    public double Area
+    {
+        get { return CalculateArea(); }
+    }
+
+    
+    public double Perimeter
+    {
+        get { return CalculatePerimeter(); }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("Inpute first length : ");
+        double side1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Inpute second lenght 2: ");
+        double side2 = Convert.ToDouble(Console.ReadLine());
+
+        // Create rectangle class
+        Rectangle rectangle = new Rectangle(side1, side2);
+
+        
+        Console.WriteLine($"perimeter of a rectangle: {rectangle.Perimeter}");
+        Console.WriteLine($"Square of a rectangle: {rectangle.Area}");
+
+        Console.ReadLine(); 
+    }
+}
